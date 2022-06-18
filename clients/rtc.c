abea39a31e2948ac8a2d0552d42f77a91f3f834b
Dan Drown <dan-android@drown.org>
2022-06-17 22:14:45 -0500

    updating rtc adjust/offset for micro tempcomp

--- clients/rtc.c
+++ clients/rtc.c
@@ -83,9 +83,9 @@ static void get(int fd) {
   median = poll_median(fd);
 
   printf("raw: ss_d = %u ss = %u dt = %u y = %u\n", page4.subsecond_div, page4.subseconds, page4.datetime, page4.year);
-  printf("timedate: %u:%u:%u %u/%u/%u\n", 
-      now.tm_hour, now.tm_min, now.tm_sec,
-      now.tm_mon+1, now.tm_mday, now.tm_year+1900
+  printf("timedate: %04u-%02u-%02u %02u:%02u:%02u\n", 
+      now.tm_year+1900, now.tm_mon+1, now.tm_mday,
+      now.tm_hour, now.tm_min, now.tm_sec
       );
   ppm = lse_calibration_to_ppm(page4.lse_calibration);
   printf("calibrate = %u (%.3f ppm) set = %u\n", page4.lse_calibration, ppm, page4.set_rtc);
@@ -258,29 +258,12 @@ static void setcalibration(int fd, uint32_t addclk, uint32_t subclk) {
   printf("set to %u (%.3f ppm)\n", page4.lse_calibration, lse_calibration_to_ppm(page4.lse_calibration));
 }
 
-float read_tcxo_ppm() {
-  FILE *f;
-  float ppm;
-
-  f = fopen("/run/tcxo-freq","r");
-  if(f == NULL) {
-    perror("fopen /run/tcxo-freq");
-    exit(1);
-  }
-  fscanf(f, "%f", &ppm);
-  fclose(f);
-
-  return ppm;
-}
-
 void compare(int fd) {
   struct timeval rtc;
   struct i2c_registers_type_page4 page4, last_page4;
   float ppm_remainder = 0.0;
 
   memset(&last_page4, '\0', sizeof(last_page4));
-
-  read_tcxo_aging();
 
   printf("LSE vs TCXO\n");
   printf("time offset ms tim2 tim14 tim2_vs_tim14 ppm tcxo lse_set lse_remainder aging\n");
@@ -298,7 +281,7 @@ void compare(int fd) {
       uint32_t milli_diff, tim2_diff, s;
       uint16_t tim14_diff, expected_tim14_diff, calib;
       int32_t tim2_adjustment;
-      float ppm, tcxo_ppm;
+      float ppm;
 
       milli_diff = page4.LSE_millis_irq - last_page4.LSE_millis_irq;
       tim2_diff = page4.LSE_tim2_irq - last_page4.LSE_tim2_irq;
@@ -317,22 +300,20 @@ void compare(int fd) {
       s = ((milli_diff+500) / 1000); // round up at 0.5s
       ppm = (tim2_diff + tim2_adjustment)/(float)s;
       ppm = (ppm - EXPECTED_FREQ) / (float)(EXPECTED_FREQ / 1000000.0);
-      tcxo_ppm = read_tcxo_ppm() + calc_tcxo_aging();
-      ppm += tcxo_ppm;
       calib = ppm_to_lse_calibration(ppm + ppm_remainder);
       ppm_remainder += ppm - lse_calibration_to_ppm(calib);
 
       // if we actually have timestamp data
       if(milli_diff != 0) { 
-        printf("%lu %.3f %u %u %u %d %.3f %.3f %.3f %.3f %.3f\n", time(NULL), diff, milli_diff, tim2_diff, tim14_diff, tim2_adjustment, ppm, tcxo_ppm, lse_calibration_to_ppm(calib), ppm_remainder, calc_tcxo_aging());
+        printf("%lu %.3f %u %u %u %d %.3f %.3f %.3f\n", time(NULL), diff, milli_diff, tim2_diff, tim14_diff, tim2_adjustment, ppm, lse_calibration_to_ppm(calib), ppm_remainder);
         _setcalibration(fd, calib);
 
         memcpy(&last_page4, &page4, sizeof(page4));
       } else {
-        printf("%lu %.3f %u %u %u %d - %.3f - - %.3f no data\n", time(NULL), diff, milli_diff, tim2_diff, tim14_diff, tim2_adjustment, tcxo_ppm, calc_tcxo_aging());
+        printf("%lu %.3f %u %u %u %d - - - no data\n", time(NULL), diff, milli_diff, tim2_diff, tim14_diff, tim2_adjustment);
       }
     } else {
-      printf("%lu %.3f - - - - - - %.3f\n", time(NULL), diff, calc_tcxo_aging());
+      printf("%lu %.3f - - - - - -\n", time(NULL), diff);
       memcpy(&last_page4, &page4, sizeof(page4));
     }
     fflush(stdout);
@@ -345,20 +326,12 @@ void offset(int fd) {
   struct timespec timers, before_timers;
   struct i2c_registers_type_page4 page4;
   struct i2c_registers_type_page5 page5;
-  uint32_t subsecond, last_tim2, second_count;
+  uint32_t subsecond;
   int32_t rtt;
-  double rtc_ts, offset_tim2;
+  double rtc_ts;
   int64_t offset_ns;
-  uint64_t local_ts, tim2_rtc_ts, last_local_ts;
+  uint64_t local_ts, tim2_rtc_ts;
   struct tm now;
-  float tcxo_ppm;
-
-  read_tcxo_aging();
-
-  offset_tim2 = 0;
-  last_tim2 = 0;
-  last_local_ts = 0;
-  second_count = 0;
 
   while(1) {
     get_rtc(fd, &rtc, &page4);
@@ -381,27 +354,10 @@ void offset(int fd) {
     if(before_timers.tv_sec < timers.tv_sec)
       rtt += 1000000; 
 
-    printf("%.6f %.6f %.6f %d %u", local_ts/1000000000.0, offset_ns/1000000000.0, tim2_rtc_ts/1000000000.0 - rtc_ts, rtt, second_count);
+    printf("%.6f %.6f %.6f %d %u %u\n", local_ts/1000000000.0, offset_ns/1000000000.0, tim2_rtc_ts/1000000000.0 - rtc_ts, rtt, page4.tim2_rtc_second, page4.LSE_tim2_irq);
+    fflush(stdout);
 
-    tcxo_ppm = read_tcxo_ppm() + calc_tcxo_aging();
-    if(last_local_ts != 0) {
-      uint32_t cycles = page5.cur_tim2 - last_tim2;
-      double this_offset, local_delta, remote_delta;
-      
-      remote_delta = cycles / (float)EXPECTED_FREQ;
-      local_delta = (local_ts - last_local_ts)/(double)1000000000.0;
-      this_offset = (remote_delta * (1 + (tcxo_ppm / 1000000.0))) - local_delta;
-      offset_tim2 += this_offset;
-      printf(" %.9f %.9f %.3f %.9f\n", offset_tim2, this_offset, tcxo_ppm, subsecond / 48000000.0);
-    } else {
-      printf("\n");
-    }
-
-    last_tim2 = page5.cur_tim2;
-    last_local_ts = local_ts;
-    second_count++;
-
-    sleep(1);
+    sleep(60);
   }
 }
 
